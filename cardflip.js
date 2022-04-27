c73450c91be32a7f6509affa57d75296ee1a9fb9
Suleenpb <suleen.goh@gmail.com>
2022-04-27 20:28:43 +0800

    mg

--- cardflip.js
+++ cardflip.js
@@ -1,1 +1,9 @@
-
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css"></link>
+        <title>Flip Card Memory Game</title>
+    </head>
+    <body>
+        <script src="./src/flipcard.js"></script>
+    </body>
+</html>
